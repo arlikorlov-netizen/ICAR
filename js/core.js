718a26615eaf29418eec8f1f7139bab9ae590fab
Jan Templar <arlik.orlov@gmail.com>
2025-12-04 17:13:39 +0300

    Ещё чуть поднимаем

--- js/core.js
+++ js/core.js
@@ -6,7 +6,7 @@
 // === БЛОК 19.1: Конфигурация приложения ===
 const AppConfig = {
     userName: "ICAR",
-    userLevel: 20,
+    userLevel: 21,
     progressValues: {
         physical: 56,
         mental: 81,
