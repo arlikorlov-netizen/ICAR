4572bae3962e1532e28387821dbcdd9752f031bd
Jan Templar <arlik.orlov@gmail.com>
2025-12-05 19:56:22 +0300

    добейте меня а

--- js/core.js
+++ js/core.js
@@ -7,7 +7,7 @@
 const AppConfig = {
     userName: "ICAR",
     userLevel: 5,
-    version: "1.0.91", // ← Добавляем версию
+    version: "1.0.92", // ← Добавляем версию
     commitHash: "a1b2c3d", // ← Добавляем хэш коммита
     progressValues: {
         physical: 56,
