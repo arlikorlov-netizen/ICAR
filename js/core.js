d613fd9429fc4efaf2e1b3624eccb0867a92141a
Jan Templar <arlik.orlov@gmail.com>
2025-12-05 13:49:48 +0300

    Начинаем двигать точки

--- js/core.js
+++ js/core.js
@@ -6,7 +6,7 @@
 // === БЛОК 19.1: Конфигурация приложения ===
 const AppConfig = {
     userName: "ICAR",
-    userLevel: 28,
+    userLevel: 29,
     progressValues: {
         physical: 56,
         mental: 81,
