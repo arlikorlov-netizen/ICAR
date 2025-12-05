4fd3909bef54c1eec5702051adf63d5ded8ee06f
Jan Templar <arlik.orlov@gmail.com>
2025-12-05 18:32:32 +0300

    Пробуем ещё раз углы

--- js/core.js
+++ js/core.js
@@ -7,7 +7,7 @@
 const AppConfig = {
     userName: "ICAR",
     userLevel: 5,
-    version: "1.0.9", // ← Добавляем версию
+    version: "1.0.10", // ← Добавляем версию
     commitHash: "a1b2c3d", // ← Добавляем хэш коммита
     progressValues: {
         physical: 56,
