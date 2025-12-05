7e179644d06e9afb2dd20c019ddbcafce7c30ef7
Jan Templar <arlik.orlov@gmail.com>
2025-12-05 11:22:54 +0300

    Возращаем нижние ярлычки на телефоне

--- js/core.js
+++ js/core.js
@@ -6,7 +6,7 @@
 // === БЛОК 19.1: Конфигурация приложения ===
 const AppConfig = {
     userName: "ICAR",
-    userLevel: 21,
+    userLevel: 22,
     progressValues: {
         physical: 56,
         mental: 81,
