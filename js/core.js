74b2336b391c0d35cc9991ff99367762a50bf58f
Jan Templar <arlik.orlov@gmail.com>
2025-12-05 12:06:44 +0300

    Слишком низко уехали нижнее

--- js/core.js
+++ js/core.js
@@ -6,7 +6,7 @@
 // === БЛОК 19.1: Конфигурация приложения ===
 const AppConfig = {
     userName: "ICAR",
-    userLevel: 25,
+    userLevel: 26,
     progressValues: {
         physical: 56,
         mental: 81,
