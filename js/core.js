cc2e4c3b1c5cbe148ff63e10bf401ed9473fe05b
Jan Templar <arlik.orlov@gmail.com>
2025-12-08 12:09:39 +0300

    Фиксим панельки

--- js/core.js
+++ js/core.js
@@ -7,7 +7,7 @@
 const AppConfig = {
     userName: "ICAR",
     userLevel: 5,
-    version: "1.0.93", // ← Добавляем версию
+    version: "1.0.94", // ← Добавляем версию
     commitHash: "a1b2c3d", // ← Добавляем хэш коммита
     progressValues: {
         physical: 56,
