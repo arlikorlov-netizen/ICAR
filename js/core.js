5e52c8f1d654364b18aa26c068d1580f1eb70113
Jan Templar <arlik.orlov@gmail.com>
2025-12-09 17:44:17 +0300

    Пытаемся вернуть крестики

--- js/core.js
+++ js/core.js
@@ -22,7 +22,7 @@
 const AppConfig = {
     userName: "ICAR",
     userLevel: 5,
-    version: "1.0.119", // ← Добавляем версию
+    version: "1.0.120", // ← Добавляем версию
     commitHash: "a1b2c3d", // ← Добавляем хэш коммита
     progressValues: {
         physical: 56,
