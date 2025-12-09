b2502c5befa3a3011c35913cd4bdc1691344d4da
Jan Templar <arlik.orlov@gmail.com>
2025-12-09 15:45:56 +0300

    Пробуем убрать рамку при всех панелях

--- js/core.js
+++ js/core.js
@@ -7,7 +7,7 @@
 const AppConfig = {
     userName: "ICAR",
     userLevel: 5,
-    version: "1.0.110", // ← Добавляем версию
+    version: "1.0.111", // ← Добавляем версию
     commitHash: "a1b2c3d", // ← Добавляем хэш коммита
     progressValues: {
         physical: 56,
